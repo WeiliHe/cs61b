d0f56064ad2c61f4900f92a3197ac326f756278c
WeiliHe <weilihe@berkeley.edu>
2019-03-24 12:23:43 +0800

    modify testGold style

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,72 +1,13 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * @Author Weili
+ * @Source StudentArrayDequeLauncher
+ */
 public class TestArrayDequeGold {
     @Test
     public void testArrayDeque() {
-        // StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-        // ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
-        // // addLast test
-        // for (int i = 0; i < 10; i += 1) {
-        //     int numberBetweenZeroAndTen = StdRandom.uniform(11);
-        //     sad1.addLast(numberBetweenZeroAndTen);
-        //     ads1.addLast(numberBetweenZeroAndTen);
-        // }
-        // // System.out.println("student array");
-        // // sad1.printDeque();
-        // // System.out.println("solution array");
-        // // ads1.printDeque();
-        // // get the item and see whether they ara equal
-        // for (int i = 0; i < 10; i += 1) {
-        //     int actual = sad1.get(i);
-        //     int expected = ads1.get(i);
-        //     assertEquals("student answer was" + actual + "correct one should be" + expected, expected, actual);
-        // }
-
-
-        // // addFirst Test
-        // StudentArrayDeque<Integer> sad2 = new StudentArrayDeque<>();
-        // ArrayDequeSolution<Integer> ads2 = new ArrayDequeSolution<>();
-        // for (int i = 0; i < 10; i += 1) {
-        //     int numberBetweenZeroAndTen = StdRandom.uniform(11);
-        //     sad2.addFirst(numberBetweenZeroAndTen);
-        //     ads2.addFirst(numberBetweenZeroAndTen);
-        // }
-        // // System.out.println("student array");
-        // // sad1.printDeque();
-        // // System.out.println("solution array");
-        // // ads1.printDeque();
-        // // get the item and see whether they ara equal
-        // for (int i = 0; i < 10; i += 1) {
-        //     int actual = sad1.get(i);
-        //     int expected = ads1.get(i);
-        //     assertEquals("student answer was" + actual + "correct one should be" + expected, expected, actual);
-        // }
-
-        // // random addLast addFirst
-        // StudentArrayDeque<Integer> sad3 = new StudentArrayDeque<>();
-        // ArrayDequeSolution<Integer> ads3 = new ArrayDequeSolution<>();
-        // // addLast test
-        // for (int i = 0; i < 10; i += 1) {
-        //     double numberBetweenZeroAndOne = StdRandom.uniform();  
-        //     if (numberBetweenZeroAndOne < 0.5) {
-        //     sad3.addFirst(i);
-        //     ads3.addFirst(i);                
-        //     } else {
-        //         sad3.addLast(i);
-        //         ads3.addLast(i); 
-        //     }
-        // }
-        // // System.out.println("student array");
-        // // sad3.printDeque();
-        // // System.out.println("solution array");
-        // // ads3.printDeque();
-        // // get the item and see whether they ara equal
-        // for (int i = 0; i < 10; i += 1) {
-        //     int actual = sad3.get(i);
-        //     int expected = ads3.get(i);
-        //     assertEquals("student answer was" + actual + "correct one should be" + expected, expected, actual);
-        // }
 
         // test remove
         StudentArrayDeque<Integer> sad4 = new StudentArrayDeque<>();
@@ -75,18 +16,14 @@ public class TestArrayDequeGold {
         for (int i = 0; i < 20; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();  
             if (numberBetweenZeroAndOne < 0.5) {
-            sad4.addFirst(i);
-            ads4.addFirst(i);                
+                sad4.addFirst(i);
+                ads4.addFirst(i);
             } else {
                 sad4.addLast(i);
                 ads4.addLast(i); 
             }
         }
-        // System.out.println("student array");
-        // sad4.printDeque();
-        // System.out.println("solution array");
-        // ads4.printDeque();
-        // get the item and see whether they ara equal
+
         StudentArrayDeque<Integer> fromsad4 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> fromads4 = new ArrayDequeSolution<>();
         for (int i = 0; i < 10; i += 1) {
@@ -109,23 +46,17 @@ public class TestArrayDequeGold {
             int actual = sad4.get(i);
             int expected = ads4.get(i);
             assertEquals(expected, actual);
-            assertEquals("student answer was" + actual + "correct one should be" + expected, expected, actual);
-        }
-
-        for (int i = 0; i < 10; i += 1) {
-            int actual = fromsad4.get(i);
-            int expected = fromsad4.get(i);
-            assertEquals("student answer was" + actual + "correct one should be" + expected, expected, actual);
+            assertEquals("student answer was" + actual + "correct one should be" + expected,
+                    expected, actual);
         }
 
         for (int i = 0; i < 10; i += 1) {
             Integer actual = sad4.removeFirst();
             Integer expected = ads4.removeFirst();
             // System.out.println(i);
-            assertEquals("student answer was" + actual + "correct one should be" + expected, expected, actual);
+            assertEquals("student answer was" + actual + "correct one should be" + expected,
+                    expected, actual);
         }
-        System.out.println("student array");
-        sad4.printDeque();
     }
 
     @Test
@@ -136,11 +67,11 @@ public class TestArrayDequeGold {
         for (int i = 0; i < 20; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();  
             if (numberBetweenZeroAndOne < 0.5) {
-            studentArr.addFirst(i);
-            solutionArr.addFirst(i);                
+                studentArr.addFirst(i);
+                solutionArr.addFirst(i);
             } else {
                 studentArr.addLast(i);
-                 solutionArr.addLast(i); 
+                solutionArr.addLast(i);
             }
         }
         System.out.println("student array");
@@ -153,8 +84,9 @@ public class TestArrayDequeGold {
             Integer actual = studentArr.removeLast();
             Integer expected = solutionArr.removeLast();
             // System.out.println(i);
-            assertEquals("student answer was" + actual + "correct one should be" + expected, expected, actual);
+            assertEquals("student answer was" + actual + "correct one should be" + expected,
+                    expected, actual);
         }
-
     }
+
 } 
