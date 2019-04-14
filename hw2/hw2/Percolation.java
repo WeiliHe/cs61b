ebba07b5cf3f86bf47a0274e90ee97fbebbbcbf7
WeiliHe <weilihe@berkeley.edu>
2019-04-14 16:31:27 +0800

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,28 +1,111 @@
-package hw2;
+ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import java.lang.*;
+import java.util.ArrayList;
 
 public class Percolation {
-    WeightedQuickUnionUF net;
+    private WeightedQuickUnionUF openSites;
+    private WeightedQuickUnionUF antiBackwash;
+    private int[][] sqNetwork;
+    private int numofOpenSites;
+    private int virtualTop;
+    private int virtualBottom;
+    private int netSize;
+
     public Percolation(int N) {
-        net = new WeightedQuickUnionUF(N);
+        openSites = new WeightedQuickUnionUF(N*N + 2);
+        antiBackwash = new WeightedQuickUnionUF(N*N + 1);
+        sqNetwork = new int[N][N];
+        numofOpenSites = 0;
+        virtualTop = N*N;
+        virtualBottom = N*N + 1;
+        // initializeUnionSet(N);
+        netSize = N;
     }
+
+    private void initializeUnionSet(int N){
+    	for (int i = 0; i < N; i++) {
+    		openSites.union(xyTo1D(0, i), virtualTop);
+    		openSites.union(xyTo1D(N-1, i), virtualBottom);
+    		antiBackwash.union(xyTo1D(0, i), virtualTop);
+    	}
+    }
+
+    // convert 2D index to 1D
+    private int xyTo1D(int r, int c) {
+    	return r * netSize +  c;
+    }
+
+    // find the nearby open node
+    private ArrayList<Integer> getOpenNearby(int row, int col) {
+    	ArrayList<Integer> openNeighbors = new ArrayList();
+    	if (row > 0) {
+            if (isOpen(row - 1, col)) {
+                openNeighbors.add(xyTo1D(row - 1, col));
+            } 
+        }
+        if (row < netSize - 1) {
+            if (isOpen(row + 1, col)) {
+                openNeighbors.add(xyTo1D(row + 1, col));
+            } 
+        }
+        if (col > 0) {
+        	if (isOpen(row, col - 1)) {
+                openNeighbors.add(xyTo1D(row, col - 1));
+            } 
+        }
+       	if (col < netSize - 1) {
+        	if (isOpen(row, col + 1)) {
+                openNeighbors.add(xyTo1D(row, col + 1));
+            } 
+        }
+    	return openNeighbors;
+    } 
+
     public void open(int row, int col) {
-        return;
+        if (isOpen(row, col)) {
+            return;
+        }
+        if ((row < 0 | row > netSize - 1) | (col < 0 | col > netSize - 1)) {
+            throw new IllegalArgumentException();
+        }
+        if (row == 0) {
+        	openSites.union(virtualTop, xyTo1D(row, col));
+            antiBackwash.union(virtualTop, xyTo1D(row, col));
+        } else if (row == netSize - 1) {
+        	openSites.union(virtualBottom, xyTo1D(row, col));
+        }
+
+        sqNetwork[row][col] = 1;
+        numofOpenSites += 1;
+        // if nearbyNode is open, connect
+        ArrayList<Integer> openNeighbors = getOpenNearby(row, col);
+        for (Integer openNeighbor: openNeighbors) {
+        	if (openNeighbor != null) {
+	            openSites.union(openNeighbor, xyTo1D(row, col));
+	            antiBackwash.union(openNeighbor, xyTo1D(row, col));
+        	}
+        }
     }
-    public boolean isOpen(int row, int col) {
-        return false;
+
+    public boolean isOpen(int r, int c) {
+        return sqNetwork[r][c] == 1;
     }
-    public boolean isFull(int row, int col) {
-        return false;
+
+    public boolean isFull(int r, int c) {
+        return antiBackwash.connected(xyTo1D(r, c), virtualTop);
     }
+
     public int numberOfOpenSites() {
-        return 0;
+        return numofOpenSites;
     }
+
     public boolean percolates() {
-        return false;
+        return openSites.connected(virtualTop, virtualBottom);
     }
     public static void main (String[] args) {
-
+  //       int N = 20;
+		// Percolation net = new Percolation(N);
     }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,46 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import edu.princeton.cs.introcs.Stopwatch;
+import java.lang.Math;
+import java.lang.*;
 
 public class PercolationStats {
+    private int[] xArray;
+    private int netSize;
+    private int simTimes;
+    final static double cfInterv = 1.96;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 | T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        Stopwatch watch = new Stopwatch();
+        for (int t = 0; t < T; t++) {
+            Percolation net = pf.make(N);
+            int x = 0;
+            while (net.percolates() != true) {
+//                generate the random row and col
+                int row = StdRandom.uniform(0, N - 1);
+                int col = StdRandom.uniform(0, N - 1);
+                net.open(row, col);
+                x++;
+            }
+            xArray[t] = x;
+        }
+        System.out.println(watch.elapsedTime());
+    }
+    public double mean() {
+        return StdStats.mean(xArray);
+    }
+    public double stddev() {
+        return StdStats.stddev(xArray);
+    }
 
+    public double confidenceLow() {
+        return mean() - cfInterv * stddev() / Math.sqrt(xArray.length);
+    }
+
+    public double confidenceHigh() {
+        return mean() + cfInterv * stddev() / Math.sqrt(xArray.length);
+    }
 }
