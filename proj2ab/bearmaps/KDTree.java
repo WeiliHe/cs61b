93bb1c06d33fa150f6d8bd03ef19d72566e9b3a6
Weili <WeiliHe@sf-express.com>
2019-05-16 11:42:45 +0800

    commit

--- proj2ab/bearmaps/KDTree.java
+++ proj2ab/bearmaps/KDTree.java
@@ -2,6 +2,48 @@ package bearmaps;
 import java.util.List;
 
 public class KDTree{
+    private Node root;
+    private int size;
+
+    public KDTree(List<Point> points) {
+        if (points == null) {
+            throw new IllegalArgumentException("points null")
+        }
+        for (Point point: points) {
+            if (root == null) {
+                root = new Node(point, 0);
+            }
+            else {
+
+            }
+        }
+    }
+
+    private void inser
+
+    private void insert(Point point){
+
+    }
+
+
+
+    private class Node {
+        int partitionDimention;
+        Point point;
+        Node left, right;
+        int size;
+
+        public Node(Point point, int size){
+            this.point = point;
+            this.left = null;
+            this.right = null;
+            this.partitionDimention = 0;
+            this.size = size;
+        }
+
+
+    }
+
 
 }
 
