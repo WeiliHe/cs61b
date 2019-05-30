357135ab9781321327c2f630b85d67db28fa3e38
Weili <WeiliHe@sf-express.com>
2019-05-30 17:27:07 +0800

    update hw4

--- hw4/bearmaps/hw4/AstarSolver.java
+++ hw4/bearmaps/hw4/AstarSolver.java
@@ -3,33 +3,97 @@ package bearmaps.hw4;
 import bearmaps.proj2ab.ArrayHeapMinPQ;
 import edu.princeton.cs.algs4.Stopwatch;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex>{
     private SolverOutcome outcome;
-    private double solutionWeight;
-    private List<Vertex> solution;
+    private double solutionWeight = 0;
+    private List<Vertex> solution = new ArrayList<>();
     private double timeSpent;
+    private int numStates = 0;
     private HashMap<Vertex, Vertex> edgeTo = new HashMap<>();
     private HashMap<Vertex, Double> distTo = new HashMap<>();
-    private HashMap<Vertex, Boolean> marked = new HashMap<>();
+    private HashSet<Vertex> marked = new HashSet<>();
     private ArrayHeapMinPQ<Vertex> fringe = new ArrayHeapMinPQ<>();
+    private Vertex start;
+    private Vertex end;
 
     public AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeout) {
         Stopwatch sw = new Stopwatch();
+        this.start = start;
+        this.end = end;
         distTo.put(start, 0.0);
         fringe.add(start, distTo.get(start));
-        while (fringe.size() != 0) {
+        while (fringe.size() != 0 && sw.elapsedTime() < timeout) {
             Vertex v = fringe.removeSmallest();
-            WeightedEdge<Vertex> e = input.neighbors(v);
+            numStates += 1;
+            if (v.equals(end)) {
+                outcome = SolverOutcome.SOLVED;
+                timeSpent = sw.elapsedTime();
+                solutionWeight = distTo.get(v);
+                return;
+            }
+            marked.add(v);
+//          relax all edges outgoing from v
+            List<WeightedEdge<Vertex>> neighborEdges = input.neighbors(v);
+            for (WeightedEdge e: neighborEdges){
+                Vertex eTo = (Vertex) e.to();
+                Vertex eFrom = (Vertex) e.from();
+                if (marked.contains(eTo)){
+                    continue;
+                }
+                if (!distTo.containsKey(eTo)){
+                    double dist = distTo.get(eFrom) + e.weight();
+                    distTo.put(eTo, dist);
+                    edgeTo.put(eTo, eFrom);
+                    fringe.add(eTo, dist + input.estimatedDistanceToGoal(eTo, end));
+                } else {
+                    double dist = distTo.get(eFrom) + e.weight();
+                    if (distTo.get(eTo) > dist) {
+                        distTo.put(eTo, dist);
+                        edgeTo.put(eTo, eFrom);
+                        fringe.changePriority(eTo, dist + input.estimatedDistanceToGoal(eTo, end));
+                    }
+                }
+            }
+        }
+        if (sw.elapsedTime() >= timeout) {
+            outcome = SolverOutcome.TIMEOUT;
+            timeSpent = timeout;
+            solution = getSolution();
+        } else {
+            outcome = SolverOutcome.UNSOLVABLE;
+            timeSpent = sw.elapsedTime();
         }
     }
-    public SolverOutcome outcome() {
 
+    private List<Vertex> getSolution() {
+        if (outcome != SolverOutcome.SOLVED) {
+            return new ArrayList<>();
+        }
+        List<Vertex> path = new ArrayList<>();
+        for (Vertex x = this.end; x != this.start; x = edgeTo.get(x)) {
+            path.add(x);
+        }
+        path.add(this.start);
+        Collections.reverse(path);
+        return path;
     }
-    public List<Vertex> solution()
-    public double solutionWeight()
-    public int numStatesExplored()
-    public double explorationTime()
+
+    public SolverOutcome outcome() {
+        return outcome;
+    }
+    public List<Vertex> solution() {
+        solution = getSolution();
+        return solution;
+    }
+    public double solutionWeight() {
+        return solutionWeight;
+    }
+    public int numStatesExplored() {
+        return numStates;
+    }
+    public double explorationTime() {
+        return timeSpent;
+    }
 }
