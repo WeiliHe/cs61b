3a1c783f461f994c5498791afe07e512b087664f
Weili <WeiliHe@sf-express.com>
2019-07-29 19:01:26 +0800

    update phase2

--- proj3/byow/Core/Main.java
+++ proj3/byow/Core/Main.java
@@ -1,5 +1,9 @@
 package byow.Core;
 
+import byow.SaveDemo.Editor;
+
+import java.io.*;
+
 /** This is the main entry point for the program. This class simply parses
  *  the command line inputs, and lets the byow.Core.Engine class take over
  *  in either keyboard or input string mode.
@@ -18,4 +22,47 @@ public class Main {
             engine.interactWithKeyboard();
         }
     }
+
+    // load the saving WorldGenerator
+    private static Interaction loadEditor() {
+        File f = new File("./save_data");
+        if (f.exists()) {
+            try {
+                FileInputStream fs = new FileInputStream(f);
+                ObjectInputStream os = new ObjectInputStream(fs);
+                return (Interaction) os.readObject();
+            } catch (FileNotFoundException e) {
+                System.out.println("file not found");
+                System.exit(0);
+            } catch (IOException e) {
+                System.out.println(e);
+                System.exit(0);
+            } catch (ClassNotFoundException e) {
+                System.out.println("class not found");
+                System.exit(0);
+            }
+        }
+        /* In the case no Editor has been saved yet, we return a new one. */
+        System.exit(0);
+        return new Interaction();
+    }
+
+    private static void saveEditor(Interaction player) {
+        File f = new File("./save_data");
+        try {
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+            FileOutputStream fs = new FileOutputStream(f);
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(player);
+        }  catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
 }
--- proj3/byow/Core/Point.java
+++ proj3/byow/Core/Point.java
@@ -7,6 +7,13 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+    public void setX (int x) {
+        this.x = x;
+    }
+
+    public void setY (int y) {
+        this.y = y;
+    }
     public int x(){
         return this.x;
     }
